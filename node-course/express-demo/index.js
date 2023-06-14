3316f80706abb7d00f84f91a7d202ab1a9abcc81
Kowit Thanapruettiwong <workframe4472@gmail.com>
2023-06-14 16:20:44 +0700

    put post get

--- node-course/express-demo/index.js
+++ node-course/express-demo/index.js
@@ -26,25 +26,55 @@ app.post("/api/courses", (req, res) => {
   const result = Joi.validate(req.body, schema);
 
   if (result.error) {
-    // 400 Bad Request
-    res.status(400).send(result.error.details[0].message);
+    // 422 Unprocessable Entity
+    res.status(422).send(result.error.details[0].message);
     return;
   }
 
   const course = {
-    id: course.length + 1,
+    id: courses.length + 1,
     name: req.body.name,
   };
-  course.push(course);
+  courses.push(course);
   res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up the course
+  // If not existing, return 422 (Unprocessable Entity)
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(422).send("The course with the given ID was not found.");
+  }
+
+  // Validate
+  // If invalid, return 400 - Bad Request
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // Update course
+  // Return the updated course
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(course, schema);
+}
 
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("Thr courese with the given ID was not found.");
+    res.status(404).send("The course with the given ID was not found.");
   res.send(course);
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Lisening on port ${port}...`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
