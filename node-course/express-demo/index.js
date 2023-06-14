f50049937096c63fe8f9aa79cdf09c2c588f53b3
Kowit Thanapruettiwong <workframe4472@gmail.com>
2023-06-14 17:39:27 +0700

    add

--- node-course/express-demo/index.js
+++ node-course/express-demo/index.js
@@ -1,8 +1,29 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
+const logger = require("./logger");
 const Joi = require("joi");
 const express = require("express");
 const app = express();
 
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`app: ${app.get("env")}`);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
+
+app.use(logger);
+
+app.use(function (req, res, next) {
+  console.log("Authenticating...");
+  next();
+});
 
 const courses = [
   { id: 1, name: "course1" },
@@ -25,11 +46,9 @@ app.post("/api/courses", (req, res) => {
 
   const result = Joi.validate(req.body, schema);
 
-  if (result.error) {
+  if (result.error)
     // 422 Unprocessable Entity
-    res.status(422).send(result.error.details[0].message);
-    return;
-  }
+    return res.status(422).send(result.error.details[0].message);
 
   const course = {
     id: courses.length + 1,
@@ -43,21 +62,30 @@ app.put("/api/courses/:id", (req, res) => {
   // Look up the course
   // If not existing, return 422 (Unprocessable Entity)
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    return res.status(422).send("The course with the given ID was not found.");
-  }
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
 
   // Validate
   // If invalid, return 400 - Bad Request
   const { error } = validateCourse(req.body);
 
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+  if (error) return res.status(400).send(error.details[0].message);
 
   // Update course
   // Return the updated course
   course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    return res.status(404).send("The course with the given ID was not found.");
+
+  // Delete
+  course.index = courses.indexOf(course);
+  courses.splice(index, 1);
+
   res.send(course);
 });
 
@@ -72,7 +100,7 @@ function validateCourse(course) {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course)
-    res.status(404).send("The course with the given ID was not found.");
+    return res.status(404).send("The course with the given ID was not found.");
   res.send(course);
 });
 
