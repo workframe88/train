d18f050eab4cdd034ab3d42e860b47bd845c926c
Kowit Thanapruettiwong <workframe4472@gmail.com>
2023-06-14 15:36:37 +0700

    postman

--- node-course/express-demo/index.js
+++ node-course/express-demo/index.js
@@ -1,12 +1,51 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!!");
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send(courses);
 });
 
-app.listen(3000, () => console.log("Lisening on port 3000..."));
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  const result = Joi.validate(req.body, schema);
+  console.log(result);
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res
+      .status(400)
+      .send("Name is required and should be minimum 3 characters.");
+  }
+
+  const course = {
+    id: course.length + 1,
+    name: req.body.name,
+  };
+  course.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("Thr courese with the given ID was not found.");
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Lisening on port ${port}...`));
