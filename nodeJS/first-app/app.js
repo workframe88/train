e7c50759703334fbd35effb0f11ab6fdc43110a4
Kowit Thanapruettiwong <workframe4472@gmail.com>
2023-06-14 10:22:00 +0700

    Update app.js

--- nodeJS/first-app/app.js
+++ nodeJS/first-app/app.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
 
-const filse = fs.readdirSync("./");
-// console.log(filse);
-
-fs.readdir("./", function (err, files) {
-  if (err) console.log("Error", err);
-  else console.log("Result", filse);
+emitter.on("messageLogged", function () {
+  console.log("Listener called");
 });
+
+emitter.emit("messageLogged");
